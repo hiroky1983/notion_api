3654853c49990a41376c64ee56ff458be5fed2e7
yamadahiroki <hirockysan1983@gmail.com>
2022-05-26 15:05:27 +0900

    notionのapiを使ってみた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import axios from "../libs/axios";
+import { databaseId, notion } from "../libs/notion";
 
-const Home: NextPage = () => {
+const Home: NextPage<any> = ({ props }) => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <main>
@@ -8,6 +11,18 @@ const Home: NextPage = () => {
       </main>
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const responseA = await notion.databases.retrieve({
+    database_id: databaseId || "",
+  });
+
+  return {
+    props: {
+      response: responseA.properties,
+    },
+  };
 };
 
 export default Home;
