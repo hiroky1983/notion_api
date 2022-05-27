c372b00199ab48475aebf4caa3a45b9e94620321
yamadahiroki <hirockysan1983@gmail.com>
2022-05-28 07:11:00 +0900

    notion apiをフロントで取得

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
-import { NextPage } from "next";
+import { QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
+import { GetStaticProps, NextPage } from "next";
 import { useEffect, useState } from "react";
 import axios from "../libs/axios";
 import { databaseId, notion } from "../libs/notion";
 
-const Home: NextPage<any> = ({ props }) => {
+type Props = {
+  response: QueryDatabaseResponse;
+};
+
+const Home: NextPage<Props> = (props) => {
+  console.log(props.response);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <main>
@@ -13,14 +20,30 @@ const Home: NextPage<any> = ({ props }) => {
   );
 };
 
-export const getStaticProps = async () => {
-  const responseA = await notion.databases.retrieve({
+export const getStaticProps: GetStaticProps = async () => {
+  const responseA = await notion.databases.query({
     database_id: databaseId || "",
+    filter: {
+      or: [
+        {
+          property: "ジャンル",
+          multi_select: {
+            contains: "日常会話",
+          },
+        },
+      ],
+    },
+    sorts: [
+      {
+        property: "ジャンル",
+        direction: "ascending",
+      },
+    ],
   });
 
   return {
     props: {
-      response: responseA.properties,
+      response: responseA.results,
     },
   };
 };
