9a1bd3b2a5206eab70b39cfcb7b39d00627bab1c
yamadahiroki <hirockysan1983@gmail.com>
2022-05-28 16:52:59 +0900

    不要コード削除など

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
 import { QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
 import { GetStaticProps, NextPage } from "next";
-import { useEffect, useState } from "react";
-import axios from "../libs/axios";
 import { databaseId, notion } from "../libs/notion";
 
 type Props = {
-  response: QueryDatabaseResponse;
+  response: QueryDatabaseResponse[];
 };
 
 const Home: NextPage<Props> = (props) => {
-  console.log(props.response);
+  console.log(props);
+
+  const A = props.response.map((item) => {
+    return item.properties;
+  });
+  console.log(A);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -21,29 +24,21 @@ const Home: NextPage<Props> = (props) => {
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const responseA = await notion.databases.query({
+  const response = await notion.databases.query({
     database_id: databaseId || "",
-    filter: {
-      or: [
-        {
-          property: "ジャンル",
-          multi_select: {
-            contains: "日常会話",
-          },
-        },
-      ],
-    },
     sorts: [
       {
-        property: "ジャンル",
+        property: "created_at",
         direction: "ascending",
       },
     ],
   });
 
+  console.log(response.results);
+
   return {
     props: {
-      response: responseA.results,
+      response: response.results,
     },
   };
 };
