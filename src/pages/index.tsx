86b659cdb7369b204d84f5b336632d129b54824f
yamadahiroki <hirockysan1983@gmail.com>
2022-05-29 02:52:54 +0900

    ジャンルリストの取得

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,28 +3,32 @@ import { GetStaticProps, NextPage } from "next";
 import { databaseId, notion } from "../libs/notion";
 
 type Props = {
-  response: QueryDatabaseResponse[];
+  response: QueryDatabaseResponse;
+  props: {
+    id: string;
+    name: string;
+    color: string;
+  }[];
 };
 
-const Home: NextPage<Props> = (props) => {
+const Home: NextPage<Props> = ({ response, props }) => {
   console.log(props);
-
-  const A = props.response.map((item) => {
-    return item.properties;
-  });
-  console.log(A);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <main>
-        <h1>Next template</h1>
+        <ul>
+          {props.map((prop) => (
+            <li key={prop.id}>{prop.name}</li>
+          ))}
+        </ul>
       </main>
     </div>
   );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await notion.databases.query({
+  const data = await notion.databases.query({
     database_id: databaseId || "",
     sorts: [
       {
@@ -33,12 +37,23 @@ export const getStaticProps: GetStaticProps = async () => {
       },
     ],
   });
+  const result = data.results;
 
-  console.log(response.results);
+  const tags = result.map((cur: any) => {
+    const tag = cur.properties["ジャンル"];
+    const tagName = tag.multi_select[0];
+    return tagName;
+  });
+
+  const newTags = tags.filter(
+    (element, index, self) =>
+      self.findIndex((e) => e.id === element.id) === index
+  );
 
   return {
     props: {
-      response: response.results,
+      response: data,
+      props: newTags,
     },
   };
 };
